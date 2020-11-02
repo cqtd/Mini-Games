f47293e6242d8627ca1fe2223ac7dd9594ea0ea1
cqtd <cqtd@protonmail.ch>
2020-11-02 12:02:12 +0900

    Json, Mirror

---
+++ Yacht Dice/Assets/Scripts/Test/Teleport.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public GameObject targetObject;
+	public GameObject toObject;
+
+	public bool availbale = false;
+
+	void Awake()
+	{
+		Monster.onMonsterExtinct += OpenPortal;
+	}
+
+	public void OpenPortal()
+	{
+		availbale = true;
+	}
+
+	void Update()
+	{
+		if (targetObject == null) return;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (availbale)
+			{
+				DoTeleport();
+			}
+			else
+			{
+				Debug.LogWarning("available 상태가 아닙니다.");
+			}
+		}
+	}
+
+	void DoTeleport()
+	{
+		targetObject.transform.position = toObject.transform.position;
+
+		availbale = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			targetObject = other.gameObject;
+			Debug.Log("OnTriggerEnter");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			targetObject = null;
+			Debug.Log("OnTriggerExit");
+		}
+	}
+}
