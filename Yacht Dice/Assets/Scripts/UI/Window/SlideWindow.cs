704fc5c3ede862447cf8f988bd9563d919590414
cqtd <cqtd@protonmail.ch>
2020-11-10 16:09:32 +0900

    어드레서블 테스트

--- Yacht Dice/Assets/Scripts/UI/Window/SlideWindow.cs
+++ Yacht Dice/Assets/Scripts/UI/Window/SlideWindow.cs
@@ -1,16 +1,30 @@
 ﻿using CQ.UI;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.AddressableAssets.ResourceLocators;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
 namespace CQ.MiniGames
 {
 	public class SlideWindow : UIWindow
 	{
 		RectTransform rect = default;
+
+		public Image avatar = default;
+
+		const string path = "Assets/Sprites/Mockups/Orono Noguchi 1.png";
 		
 		public override void InitComponent()
 		{
 			rect = transform as RectTransform;
+
+			AsyncOperationHandle<Sprite> operation = Addressables.LoadAssetAsync<Sprite>(path);
+			operation.Completed += handle =>
+			{
+				avatar.sprite = handle.Result;
+			};
 		}
 		
 		public override void Open()
