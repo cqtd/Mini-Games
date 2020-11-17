2504ae94b7a0c376ebed05ec873384ccbc7a0e55
cqtd <cqtd@protonmail.ch>
2020-11-18 02:11:55 +0900

    Add Component : UniRx

---
+++ Yacht Dice/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace CQ.MiniGames
+{
+	public class CameraTarget : MonoBehaviour
+	{
+		void OnDrawGizmos()
+		{
+			Gizmos.DrawWireSphere(transform.position, 0.2f);
+			Handles.Label(transform.position + Vector3.up * 1f, Vector3.Distance(transform.position, Camera.main.transform.position).ToString());
+		}
+	}
+}
