d35b32fb88a1271ac73098cbe5dbcfac1481a2d8
cqtd <cqtd@protonmail.ch>
2020-11-23 21:40:19 +0900

    커스텀 툴바 적용

--- Yacht Dice/Assets/Scripts/UI/YachtUIManager.cs
+++ Yacht Dice/Assets/Scripts/UI/YachtUIManager.cs
@@ -41,9 +41,6 @@ namespace CQ.MiniGames.UI
 			CreateScreenSizeChangeStream();
 			
 			Instance.Open(initialCanvas);
-			// UnityEditor.DockArea
-			// UnityEditor.EditorWindow
-			// UnityEditor.UnityMainToolbar;
 		}
 
 		public static void RegisterScreenSizeChange(Action<Vector2> callback)
