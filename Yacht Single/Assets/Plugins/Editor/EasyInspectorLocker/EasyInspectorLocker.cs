8b7df549a838f56cc730b0ee9e54dd3eb23f130e
cqtd <cqtd@protonmail.ch>
2021-01-25 21:22:50 +0900

    플러그인 폴더 정리

---
+++ Yacht Single/Assets/Plugins/Editor/EasyInspectorLocker/EasyInspectorLocker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace EasyInspectorLocker
+{
+	internal class EasyInspectorLocker
+	{
+		private static EditorWindow _mouseOverWindow;
+
+		[MenuItem("Tools/Shortcut/Lock Inspector &D")] // Alt + D
+		private static void LockInspector()
+		{
+			if (_mouseOverWindow == null)
+			{
+				if (!EditorPrefs.HasKey("LockableInspectorIndex"))
+					EditorPrefs.SetInt("LockableInspectorIndex", 0);
+				int i = EditorPrefs.GetInt("LockableInspectorIndex");
+
+				Type type = Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.InspectorWindow");
+				var findObjectsOfTypeAll = Resources.FindObjectsOfTypeAll(type);
+				_mouseOverWindow = (EditorWindow) findObjectsOfTypeAll[i];
+			}
+
+			if (_mouseOverWindow != null && _mouseOverWindow.GetType().Name == "InspectorWindow")
+			{
+				Type type = Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.InspectorWindow");
+				PropertyInfo propertyInfo = type.GetProperty("isLocked");
+				
+				if (propertyInfo != null)
+				{
+					bool value = (bool) propertyInfo.GetValue(_mouseOverWindow, null);
+					propertyInfo.SetValue(_mouseOverWindow, !value, null);
+					_mouseOverWindow.Repaint();
+				}
+			}
+		}
+		
+		[MenuItem("Tools/Shortcut/Toggle Inspector Lock %l")] // Ctrl + L
+		private static void ToggleInspectorLock()
+		{
+			ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+			ActiveEditorTracker.sharedTracker.ForceRebuild();
+		}
+	}
+}
+
---
+++ Yacht Single/Assets/Yacht/Editor/ConsoleMenu.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Yacht.Editor
+{
+    public class ConsoleMenu
+    {
+        [MenuItem("Console/Log")]
+        private static void Log()
+        {
+            Debug.Log("This is a basic log.");
+        }
+
+        [MenuItem("Console/Log Warning")]
+        private static void LogWarning()
+        {
+            Debug.LogWarning("You should care about this.");
+        }
+
+        [MenuItem("Console/Log Exception")]
+        private static void LogError()
+        {
+            Debug.LogException(new NullReferenceException());
+        }
+    }
+}
+
+namespace EFGame.Editor
+{
+    public class GameInspector : UnityEditor.Editor
+    {
+        private SerializedProperty m_sessionId = default;
+        private SerializedProperty m_localPlayer = default;
+        
+        private void OnEnable()
+        {
+            m_sessionId = serializedObject.FindProperty("m_sessionId");
+            m_localPlayer = serializedObject.FindProperty("m_localPlayer");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.PropertyField(m_sessionId);
+                EditorGUILayout.PropertyField(m_localPlayer);
+            }
+        }
+    }
+}
