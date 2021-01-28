98ef765ad7b031b942a34b95ab0a886977f12732
cqtd <cqtd@protonmail.ch>
2021-01-28 17:24:07 +0900

    readonly

---
+++ Yacht Single/Assets/Plugins/Util/ReadOnlyAttribute.cs
@@ -0,0 +1,21 @@
+﻿public enum ReadOnlyOption
+{
+	DISABLE_ALL = 0,
+	EDITABLE_PLAYMODE,
+	EDITABLE_EDITMODE,
+}
+
+public class ReadOnlyAttribute : UnityEngine.PropertyAttribute
+{
+	public readonly ReadOnlyOption option;
+	
+	public ReadOnlyAttribute() : this(ReadOnlyOption.DISABLE_ALL)
+	{
+		
+	}
+	
+	public ReadOnlyAttribute(in ReadOnlyOption option = ReadOnlyOption.DISABLE_ALL)
+	{
+		this.option = option;
+	}
+}
---
+++ Yacht Single/Assets/Yacht/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+	{
+		return EditorGUI.GetPropertyHeight(property, label, true);
+	}
+
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		bool disabled = ((ReadOnlyAttribute) attribute).option switch
+		{
+			ReadOnlyOption.DISABLE_ALL => true,
+			ReadOnlyOption.EDITABLE_PLAYMODE => !Application.isPlaying,
+			ReadOnlyOption.EDITABLE_EDITMODE => Application.isPlaying,
+			_ => true
+		};
+
+		using EditorGUI.DisabledScope scope = new EditorGUI.DisabledScope(disabled);
+		EditorGUI.PropertyField(position, property, label, true);
+	}
+}
